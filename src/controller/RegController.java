7f77931c0c9a257337956d9f09ad13c145cb215f
Julio Luque <julio.a.luque@gmail.com>
2020-04-04 20:30:24 -0300

    Crud extends DispatchAction

---
+++ src/controller/RegController.java
@@ -0,0 +1,76 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+import beans.RegForm;
+import dao.EmployeeDao;
+
+public class RegController extends DispatchAction {
+
+	public ActionForward register(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+//		Read data using request object
+		/*
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		float salary = Float.parseFloat(request.getParameter("salary"));
+		 */
+		
+//		Read data using form beans
+//		/*
+		RegForm rf = (RegForm) form;
+		int id = rf.getId();
+		String name = rf.getName();
+		String email = rf.getEmail();
+		float salary = rf.getSalary();
+//		*/
+		EmployeeDao edao = new EmployeeDao();
+		int i = edao.save(id, name, email, salary);
+		
+		if (i != 0) {
+			return mapping.findForward("success");
+		} else {
+			return mapping.findForward("fail");
+		}
+	}
+
+	
+	public ActionForward update(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+//		Read data using request object
+		/*
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		float salary = Float.parseFloat(request.getParameter("salary"));
+		 */
+		
+//		Read data using form beans
+//		/*
+		RegForm rf = (RegForm) form;
+		int id = rf.getId();
+		String name = rf.getName();
+		String email = rf.getEmail();
+		float salary = rf.getSalary();
+//		*/
+		EmployeeDao edao = new EmployeeDao();
+		int i = edao.update(id, name, email, salary);
+		
+		if (i != 0) {
+			return mapping.findForward("success");
+		} else {
+			return mapping.findForward("fail");
+		}
+	}
+
+}
---
+++ src/dao/EmployeeDao.java
@@ -0,0 +1,42 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class EmployeeDao {
+
+//	Class.forName("oracle.jdbc.OracleDriver");
+//	Connection con = DriverManager.getConnection("jdbc:oracle:thin@localhost:1521:xe", "system", "password");
+
+//	Class.forName("com.mysql.jdbc.Driver");
+//	Connection con = DriverManager.getConnection("jdbc:mysql://localhost/strutsCrud", "root", "1234");
+	
+	public int save(int id, String name, String email, float salary) throws Exception {
+
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB", "system", "password");
+		
+		PreparedStatement ps = con.prepareStatement("insert into employee values(?,?,?,?)");
+		ps.setInt(1, id);
+		ps.setString(2, name);
+		ps.setString(3, email);
+		ps.setFloat(4, salary);
+		int i = ps.executeUpdate();
+		return i;
+	}
+	
+	public int update(int id, String name, String email, float salary) throws Exception {
+
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/XEPDB", "system", "password");
+
+		PreparedStatement ps = con.prepareStatement("update employee set name=?, email=?, salary=?, where id=?");
+		ps.setString(1, name);
+		ps.setString(2, email);
+		ps.setFloat(3, salary);
+		ps.setInt(4, id);
+		int i = ps.executeUpdate();
+		return i;
+	}
+}
